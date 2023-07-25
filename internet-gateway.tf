87bdc8daf02cb56de76fc77ecbc0050164dc65c9
TOUNGA01 <franck.tounga@5thfloor.be>
2023-07-25 14:01:31 +0200

    add igw ngw subnet vpc route table

---
+++ internet-gateway.tf
@@ -0,0 +1,144 @@
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main"
+  }
+}
+
+#***************************************  Subnets  *************************************************
+
+resource "aws_subnet" "public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.0.0/18"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name                        = "public-eu-west-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.64.0/18"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name                        = "public-eu-west-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.128.0/18"
+  availability_zone = "eu-west-1a"
+  tags = {
+    Name                              = "private-eu-west-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.192.0/18"
+  availability_zone = "eu-west-1b"
+  tags = {
+    Name                              = "private-eu-west-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+
+#***************************************  Nat Gateway  *************************************************
+
+resource "aws_eip" "nat1" {
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_eip" "nat2" {
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "gw1" {
+  subnet_id     = aws_subnet.public-1.id
+  allocation_id = aws_eip.nat1.id
+  tags = {
+    Name = "Nat 1"
+  }
+}
+
+resource "aws_nat_gateway" "gw2" {
+  subnet_id     = aws_subnet.public-2.id
+  allocation_id = aws_eip.nat2.id
+  tags = {
+    Name = "Nat 2"
+  }
+}
+
+#***************************************  Route table  *************************************************
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw1.id
+  }
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw2.id
+  }
+
+  tags = {
+    Name = "private2"
+  }
+}
+
+#***************************************  Route table Association *************************************************
+
+resource "aws_route_table_association" "public1" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public-1.id
+}
+
+resource "aws_route_table_association" "public2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public-2.id
+}
+
+resource "aws_route_table_association" "private1" {
+  route_table_id = aws_route_table.private1.id
+  subnet_id      = aws_subnet.private-1.id
+}
+
+resource "aws_route_table_association" "private2" {
+  route_table_id = aws_route_table.private2.id
+  subnet_id      = aws_subnet.private-2.id
+}
--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   profile = "eks"
-  region = "eu-west-1"
+  region  = "eu-west-1"
 }
 
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.21"
     }
   }
